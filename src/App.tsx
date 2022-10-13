9f3c4debfa89442326d18856d7ae5c2a7f58fba9
wxb <superwang9527@163.com>
2022-10-13 11:21:43 +0800

    feat: 实现左右行驶的车道完成

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const HEIGHT = 800;
 const ROADWIDTH = 100;
 const LIGHTALPHA = 0.06;
 const REDLIGHTTIMER = 5; // 红灯秒数
-const CATLENGTH = 50; // 车长
+const CARLENGTH = 50; // 车长
 
 class ListNode {
   val: Sprite;
@@ -39,13 +39,28 @@ class ListNode {
   }
 }
 
+interface CarData {
+  left: ListNode | null;
+  right: ListNode | null;
+  top: ListNode | null;
+  bottom: ListNode | null;
+}
+
+type Direction = 'left' | 'right' | 'top' | 'bottom';
+
 function App() {
   let [app, setApp] = useState<PIXI.Application>();
   const [state, dispatch] = useReducer(reducer, initData);
   let isRowGreen = useRef(true);
   let timerCount = useRef(0).current;
   let [texture, setTexture] = useState<TextureCacheObj>();
-  let catList = useRef<ListNode | null>(null);
+  let carList = useRef<ListNode | null>(null);
+  let carData = useRef<CarData>({
+    left: null,
+    right: null,
+    top: null,
+    bottom: null,
+  });
 
   useEffect(() => {
     let _app = new PIXI.Application({
@@ -138,84 +153,96 @@ function App() {
     if (!app || !texture) {
       return;
     }
-    loadSprite(
-      'left',
-      WIDTH - CATLENGTH / 2,
-      // WIDTH + CATLENGTH * 2,
-      (HEIGHT - ROADWIDTH) / 2 + ROADWIDTH / 4
-    );
+    addCar('left');
+    addCar('left');
+    addCar('left');
+    addCar('left');
 
-    loadSprite(
-      'left',
-      WIDTH + CATLENGTH * 2,
-      (HEIGHT - ROADWIDTH) / 2 + ROADWIDTH / 4
-    );
+    addCar('right');
+    addCar('right');
+    addCar('right');
 
-    loadSprite(
-      'left',
-      WIDTH + CATLENGTH * 3,
-      (HEIGHT - ROADWIDTH) / 2 + ROADWIDTH / 4
-    );
-
-    loadSprite(
-      'left',
-      WIDTH + CATLENGTH * 4,
-      (HEIGHT - ROADWIDTH) / 2 + ROADWIDTH / 4
-    );
-
-    let roadLightX = (WIDTH + ROADWIDTH + CATLENGTH) / 2;
+    interface RoadLightObj {
+      left: number;
+      right: number;
+    }
+    let roadLightObj: RoadLightObj = {
+      left: (WIDTH + ROADWIDTH + CARLENGTH) / 2,
+      right: (WIDTH - ROADWIDTH - CARLENGTH) / 2,
+    };
     app.ticker.add(() => {
-      let copyList = catList.current;
-      // 上一节点
-      let lastNode = null;
-      while (copyList) {
-        const sprite = copyList.val;
-        // 是否停车
-        let isStop = false;
-        // 判断是否在红绿灯右侧 5 距离大小，在判断是否为红灯
-        if (sprite.x - roadLightX <= 5 && sprite.x - roadLightX >= 0) {
-          if (!isRowGreen.current) {
+      for (let key in carData.current) {
+        let copyList = carData.current[key as keyof CarData];
+        let roadLightX = roadLightObj[key as keyof RoadLightObj];
+        let coordinate: 'x' | 'y' =
+          key === 'left' || key === 'right' ? 'x' : 'y';
+        // 上一节点
+        let lastNode = null;
+        while (copyList) {
+          const sprite = copyList.val;
+
+          // 是否停车
+          let isStop = false;
+          // 判断是否在红绿灯 5 距离大小，在判断是否为红灯
+          if (key === 'left') {
+            if (
+              sprite[coordinate] - roadLightX <= 5 &&
+              sprite[coordinate] - roadLightX >= 0
+            ) {
+              if (!isRowGreen.current) {
+                isStop = true;
+              }
+            }
+          } else if (key === 'right') {
+            if (
+              roadLightX - sprite[coordinate] <= 5 &&
+              roadLightX - sprite[coordinate] >= 0
+            ) {
+              if (!isRowGreen.current) {
+                isStop = true;
+              }
+            }
+          }
+
+          isOverScreen(key as Direction, sprite);
+
+          // 判断距离上一辆车的距离是否小于3，小于3就等于3
+          if (
+            lastNode &&
+            Math.abs(sprite[coordinate] - lastNode[coordinate]) <= CARLENGTH + 4
+          ) {
+            sprite[coordinate] =
+              key === 'left'
+                ? lastNode[coordinate] + CARLENGTH + 4
+                : key === 'right'
+                ? lastNode[coordinate] - CARLENGTH - 4
+                : sprite[coordinate];
+            if (sprite.state !== 'slowDown') {
+              sprite.speed = lastNode.speed - Math.random() * lastNode.speed;
+              sprite.state = 'slowDown';
+            }
             isStop = true;
+          } else if (
+            lastNode &&
+            Math.abs(sprite[coordinate] - lastNode[coordinate]) > CARLENGTH + 30
+          ) {
+            if (sprite.state !== 'accelerate') {
+              sprite.speed = sprite.speed + Math.random() * (4 - sprite.speed);
+              sprite.state = 'accelerate';
+            }
           }
-        }
-        // 判断汽车是否走出画面，链表中删除，并且从舞台中删除，在添加一辆车
-        if (sprite.x <= 0) {
-          catList.current = catList.current!.next;
-          app?.stage.removeChild(sprite);
-          if (catList.current) {
-            catList.current.val.speed = Math.random() * 1 + 3;
-          }
-          loadSprite(
-            'left',
-            WIDTH - CATLENGTH / 2,
-            // WIDTH + CATLENGTH * 2,
-            (HEIGHT - ROADWIDTH) / 2 + ROADWIDTH / 4
-          );
-        }
-        // 判断距离上一辆车的距离是否小于3，小于3就等于3
-        if (lastNode && Math.abs(sprite.x - lastNode.x) <= CATLENGTH + 4) {
-          sprite.x = lastNode.x + CATLENGTH + 4;
-          if (sprite.state !== 'slowDown') {
-            sprite.speed = lastNode.speed - Math.random() * lastNode.speed;
-            sprite.state = 'slowDown';
-          }
-          isStop = true;
-        } else if (
-          lastNode &&
-          Math.abs(sprite.x - lastNode.x) > CATLENGTH + 30
-        ) {
-          if (sprite.state !== 'accelerate') {
-            sprite.speed = sprite.speed + Math.random() * (4 - sprite.speed);
-            sprite.state = 'accelerate';
-          }
-        }
 
-        if (!isStop) {
-          sprite.x -= sprite.speed;
-        }
+          if (!isStop) {
+            if (key === 'left' || key === 'top') {
+              sprite[coordinate] -= sprite.speed;
+            } else {
+              sprite[coordinate] += sprite.speed;
+            }
+          }
 
-        lastNode = sprite;
-        copyList = copyList.next;
+          lastNode = sprite;
+          copyList = copyList.next;
+        }
       }
     });
   }, [app, texture]);
@@ -278,42 +305,98 @@ function App() {
     });
   };
 
-  const loadSprite = (
-    direction: 'left' | 'right' | 'top' | 'bottom',
-    x = 0,
-    y = 0
-  ) => {
+  /**
+   * 添加车辆
+   * @param direction : ;
+   * @returns
+   */
+  const addCar = (direction: Direction) => {
     if (!app) {
       return;
     }
     let sprite = new PIXI.Sprite(texture![direction]) as Sprite;
-    sprite.x = x;
-    sprite.y = y;
     sprite.id = Math.floor(Math.random() * 999999);
-    sprite.speed = !catList.current
+    sprite.speed = !carData.current[direction]
       ? Math.random() * 1 + 3
       : 1 + Math.random() * 3;
     sprite.anchor.set(0.5);
     sprite.state = 'normal';
+    // 计算车的宽高
     if (direction === 'left' || direction === 'right') {
-      sprite.width = CATLENGTH;
-      sprite.height = CATLENGTH / 1.5;
+      sprite.width = CARLENGTH;
+      sprite.height = CARLENGTH / 1.5;
     } else {
-      sprite.width = CATLENGTH / 1.5;
-      sprite.height = CATLENGTH;
+      sprite.width = CARLENGTH / 1.5;
+      sprite.height = CARLENGTH;
     }
-    app.stage.addChild(sprite);
 
     // 链表最后添加节点
-    if (catList.current) {
-      let current = catList.current;
+    if (carData.current[direction]) {
+      let current = carData.current[direction]!;
       while (current.next) {
         current = current.next!;
       }
+      switch (direction) {
+        case 'left':
+          sprite.y = (HEIGHT - ROADWIDTH) / 2 + ROADWIDTH / 4;
+          if (current.val.x >= WIDTH - CARLENGTH / 2) {
+            sprite.x = current.val.x + CARLENGTH * 1.5;
+          } else {
+            sprite.x = WIDTH - CARLENGTH / 2;
+          }
+          break;
+        case 'right':
+          sprite.y = HEIGHT / 2 + ROADWIDTH / 4;
+          if (current.val.x <= -CARLENGTH / 2) {
+            sprite.x = current.val.x - CARLENGTH * 1.5;
+          } else {
+            sprite.x = -CARLENGTH / 2;
+          }
+          console.log(sprite.x, sprite.y);
+          break;
+      }
       current.next = new ListNode(sprite);
     } else {
-      catList.current = new ListNode(sprite);
+      if (direction === 'left') {
+        sprite.y = (HEIGHT - ROADWIDTH) / 2 + ROADWIDTH / 4;
+        sprite.x = WIDTH - CARLENGTH / 2;
+      } else if (direction === 'right') {
+        sprite.y = HEIGHT / 2 + ROADWIDTH / 4;
+        sprite.x = -CARLENGTH / 2;
+      }
+      carData.current[direction] = new ListNode(sprite);
     }
+    app.stage.addChild(sprite);
+  };
+
+  /**
+   * 判断汽车是否走出画面，链表中删除，并且从舞台中删除，在添加一辆车
+   */
+  const isOverScreen = (direction: Direction, sprite: Sprite) => {
+    let isOver = false;
+    switch (direction) {
+      case 'left':
+        isOver = sprite.x <= -CARLENGTH / 2;
+        break;
+      case 'right':
+        isOver = sprite.x >= WIDTH + CARLENGTH / 2;
+        break;
+      case 'top':
+        isOver = sprite.y <= -CARLENGTH / 2;
+        break;
+      case 'bottom':
+        isOver = sprite.y <= HEIGHT + CARLENGTH / 2;
+        break;
+    }
+    if (!isOver) {
+      return;
+    }
+    carData.current[direction] = carData.current[direction]!.next;
+    app?.stage.removeChild(sprite);
+    if (carData.current[direction]) {
+      carData.current[direction]!.val.speed = Math.random() * 1 + 3;
+    }
+    addCar(direction);
   };
 
   return (
